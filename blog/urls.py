b7d9fe0d4aafab29c029da66bed9b2c565ced3f5
rjm3q <robert.mabry86@gmail.com>
2020-11-15 13:03:02 -0600

    Added BootStrap

--- blog/urls.py
+++ blog/urls.py
@@ -4,4 +4,5 @@ from . import views
 #'' makes it the home page, name should be unique for reverse lookup
 urlpatterns = [
     path('', views.home, name='blog-home'),
+    path('about/', views.about, name='blog-about'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,27 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+#dummy data is a list[] of dicts{}
+posts = [
+    {
+        'author': 'Robert JM',
+        'title': 'Blog Post 1',
+        'content': 'First post content',
+        'date_posted': 'November 15, 2020'
+
+    },
+    {
+        'author': 'Rose LM',
+        'title': 'Blog Post 2',
+        'content': 'Second post content',
+        'date_posted': 'November 16, 2020'
+
+    }
+]
 # Create your views here.
 
 #home function for user going to blog home page
+#context dict key posts allows insertion of argument in return function
 def home(request):
     context = {
         'posts': posts
@@ -12,20 +30,5 @@ def home(request):
 
 #logic for about page
 def about(request):
-    return render(request, 'blog/about.html' )
+    return render(request, 'blog/about.html', {'title': 'About'} )
 
-#dummy data
-posts = [
-    {
-        'author': 'Robert JM',
-        'title': 'Blog Post 1',
-        'content': 'First post content',
-        'date posted': 'November 15, 2020'
-    }
-    {
-        'author': 'Rose LM',
-        'title': 'Blog Post 2',
-        'content': 'Second post content',
-        'date posted': 'November 15, 2020'
-    }
-]
