f040d74ebe0021706657cb786b0b02cb551bc0fb
rjm3q <robert.mabry86@gmail.com>
2020-11-15 12:26:45 -0600

    Added Web Page templates

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,31 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+# Create your views here.
+
 #home function for user going to blog home page
 def home(request):
-    return HttpResponse('<h1>Blog Home</h1>')
-# Create your views here.
+    context = {
+        'posts': posts
+    }
+    return render(request, 'blog/home.html', context)
+
+#logic for about page
+def about(request):
+    return render(request, 'blog/about.html' )
+
+#dummy data
+posts = [
+    {
+        'author': 'Robert JM',
+        'title': 'Blog Post 1',
+        'content': 'First post content',
+        'date posted': 'November 15, 2020'
+    }
+    {
+        'author': 'Rose LM',
+        'title': 'Blog Post 2',
+        'content': 'Second post content',
+        'date posted': 'November 15, 2020'
+    }
+]
